d6ec90e1bf62b0a73192cfe9c5a9b2c8de359691
Dana Truong <tramtramphotos@gmail.com>
2023-01-03 10:51:14 -0600

    Add and organize components

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <h2>Main Component</h2>
+    )
+  }
+}
+
+export default Main
