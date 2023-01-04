7ca1759422526ba9968ece803c24d0d38bf50efa
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-04 15:30:42 -0600

    Create About component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,24 @@
-import './App.css';
+import './App.css'
 import Main from '../Main/Main'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import About from '../About'
+import APP_ROUTES from '../../utilis/constants'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+    <div className='App'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path={APP_ROUTES.ABOUT} element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
