4fde688d220b3096cf734f2a6b52c41d58e69d8f
Dana Truong <tramtramphotos@gmail.com>
2023-01-05 16:11:19 -0600

    Add placeholder text to modal pop up

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,10 +13,23 @@ const Card = () => {
       <div className="results-text">
         <p className="name-text">Golden Retriever</p>
       </div>
-      <MicroModal trigger={(open) => 
-        <button className="dog-button" onClick={open}>Click For More</button>}>
-      {(close) => 
-      <button onClick={close}>Close!</button>}
+      <MicroModal
+
+        trigger={(open) => 
+          <button className="dog-button" onClick={open}>Click For More</button>}>
+        {(close) => (
+          <div className="modal">
+            <h2>The Golden Retriever</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+              culpa qui officia deserunt mollit anim id est laborum.</p>
+            <button className="close-modal-button" onClick={close}>Close!</button>
+          </div>
+        )}
       </MicroModal>
     </div>
   )
