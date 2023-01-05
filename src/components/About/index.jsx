ddba20e0687fa50241b20c5689b5dfe3be24a02e
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-04 18:07:12 -0600

    Add initial content for about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,19 @@
 import './About.css'
+import about from '../../assets/images/about.gif'
 
 export default function About() {
   return (
-    <div className="about__bg">
-
-    <p>What's up</p>
+    <div className='about'>
+      <div className='about__container'>
+        <h1>About this app</h1>
+        <img src={about} alt='happy human with their cute dog' />
+        <p className='about__text'>
+          This will be where we put the purpose of this app, which is to help
+          people find a dog compatible with their lifestyles in order to be a forever
+          home for the pet that they adopt. We need some statistics about animals being
+          re-sheltered.
+        </p>
+      </div>
     </div>
   )
 }
