51c06ac67bb0937f122183af64895ca87fe39f54
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-04 18:05:05 -0600

    Add github routes

--- src/utilis/constants.js
+++ src/utilis/constants.js
@@ -1,5 +1,11 @@
-const APP_ROUTES = {
+export const APP_ROUTES = {
   ABOUT: `/about`,
 }
 
-export default APP_ROUTES
+export const GITHUB_ROUTES = {
+  REPO: 'https://github.com/Mrcolin99/stretch-dogs',
+  JUSTEN: 'https://github.com/justenanderson-commits',
+  TRICIA: 'https://github.com/tricia-holmes/',
+  COLIN: 'https://github.com/Mrcolin99',
+  DANA: '"https://github.com/tramtram1130"',
+}
