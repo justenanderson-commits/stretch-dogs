14a905639e77ffb051144bcb2b1ccc8b94545178
Colin Ciervo <colinciervo@@gmail.com>
2023-01-08 18:22:51 -0500

    Trying tab method again

--- cypress/e2e/questionnaire.cy.js
+++ cypress/e2e/questionnaire.cy.js
@@ -13,6 +13,13 @@ describe('question page', () => {
     it('should be able to change slider values', () => {
         cy.get('#familyRating')
         .should('have.attr', 'value', 3)
+        cy.get('body')
+        .tab()
+        .tab()
+        .tab()
+        .tab()
+        .tab()
+        .tab()
         .trigger('keydown', { keyCode: 39 })
         cy.wait(500)
         cy.get('#familyRating')
@@ -25,7 +32,7 @@ describe('question page', () => {
     it('should be able to change all slider values', () => {
         
     })
-    it('sbhould be able to submit', () => {
+    it('should be able to submit', () => {
         cy.get('.submit-button').click()
         cy.intercept({method: 'GET',
         url: 'http://localhost:3001/api/v1/dogs/3/4/5/5/5'},
