7f24b59be8d3232326053ce988319ebb3761045a
Colin Ciervo <colinciervo@@gmail.com>
2023-01-08 12:40:21 -0500

    Changed which page the test loads on

--- cypress/e2e/results.cy.js
+++ cypress/e2e/results.cy.js
@@ -1,15 +1,13 @@
 describe('results page', () => {
     beforeEach(() => {
-        cy.visit('localhost:3000/')
+        cy.visit('localhost:3000/quiz')
     })
     it('should be able to navigate to the results page', () => {
-        cy.get('.quiz-link').click()
         cy.get('.submit-button').click()
         cy.get('h2')
         .contains('Top Breeds For You')
     })
     it('should show a message when there are no results matching your quiz answers', () => {
-        cy.get('.quiz-link').click()
         cy.get('.submit-button').click()
         cy.intercept({method: 'GET',
         url: 'http://localhost:3001/api/v1/dogs/3/3/3/3/3'},
@@ -18,12 +16,11 @@ describe('results page', () => {
         .contains('Hmm, maybe you')
     })
     it('should show quiz results when you habe a matching animal', () => {
-        cy.get('.quiz-link').click()
         cy.get('.submit-button').click()
         cy.intercept({method: 'GET',
         url: 'http://localhost:3001/api/v1/dogs/3/4/5/5/5'},
         { fixture: 'dogs' })
-        cy.get('.name-test')
+        cy.get('.name-text')
         .contains('Australian Shepherd')
     })
 })
