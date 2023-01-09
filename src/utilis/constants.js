7a674a26fc9eaef40253dedb5e2d0c757117a4ab
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-09 16:42:14 -0600

    https API URL

--- src/utilis/constants.js
+++ src/utilis/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://pawfect-match-api.herokuapp.com'
+const API_URL = 'https://pawfect-match-api.herokuapp.com'
 
 export const API_ROUTES = {
   GET_RESULTS: `${API_URL}/api/v1/dogs`,
