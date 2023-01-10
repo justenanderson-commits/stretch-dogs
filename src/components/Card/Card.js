be96e9e96f558ea08d4793186a3d76c8090f0bb5
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-09 19:34:39 -0600

    Add description and replace filler text

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,8 @@ import React from 'react'
 import MicroModal from 'react-micro-modal'
 import './Card.css'
 
-const Card = ({ breed, img }) => {
+const Card = ({ breed, img, description }) => {
+  console.log(description)
   return (
     <div className='results-card'>
       <img className='dog-portrait' src={img} alt={breed} />
@@ -19,15 +20,7 @@ const Card = ({ breed, img }) => {
         {(close) => (
           <div className='modal'>
             <h2>{breed}</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <p>{description}</p>
             <button className='close-modal-button' onClick={close}>
               Close!
             </button>
