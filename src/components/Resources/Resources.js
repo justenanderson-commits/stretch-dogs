48628a52dab7c864765456d44004119ebffff3f7
Justen Anderson <justenanderson@gmail.com>
2023-01-09 18:40:08 -0700

    Update Resources styling.

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,15 +1,21 @@
 import './Resources.css'
+import dogHome from '../../assets/images/dog-home.gif'
 // import about from '../../assets/images/about.gif'
 
 export default function About() {
   return (
     <div className='resources'>
       <div className='resources__container'>
-        <h1>Getting a new pup?</h1>
-        <img src="https://m.media-amazon.com/images/I/51E4iRDq0QL._AC_SL1500_.jpg" alt='happy human with their cute dog' />
+        <h1>Bringing home a new pup?</h1>
+        <img src={ dogHome } alt='happy pup on the way home' />
         <p className='resources__text'>
-          Whether adopting or getting a new puppy, it's a big change for everyone! 
+          Whether adopting or getting a new puppy, it's a big change for everyone! Here are a few helpful tips:
+          <br/>
+          <br/>
+          <a className='link__text' href="https://thecottagemarket.com/how-to-help-adopted-dogs-adjust-and-relax-in-their-new-home/" target="_blank">Helping Your New Pup Adjust</a>
+         
         </p>
+       
       </div>
     </div>
   )
