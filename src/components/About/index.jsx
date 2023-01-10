645937a0b202def41c7930a4d6f7d8011545942e
Justen Anderson <justenanderson@gmail.com>
2023-01-09 19:01:33 -0700

    Add remaining links to resources component.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,7 +9,7 @@ export default function About() {
         <img src={about} alt='happy human with their cute dog' />
         <p className='about__text'>
           At Pawfect Match, our mission is to help
-          people find a dog compatible with their lifestyle so their pup can have a forever home. Sadly, only an estimated 10% od dogs born today will find a permanent home. Around 3.9 million dogs are abandoned or given up to shelters each year, and about 30% of dogs of them are purebreds. Check our our <a href="./quiz">breed finder quiz</a> to learn if one is right for you!
+          people find a dog compatible with their lifestyle, so their pup can have a forever home. Sadly, only an estimated 10% of dogs born today will find a permanent home. Around 3.9 million dogs are abandoned or given up to shelters each year, and about 30% of dogs of them are purebreds. Check out our <a href="./quiz">breed finder quiz</a> to learn if one is right for you!
         </p>
       </div>
     </div>
--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -4,18 +4,47 @@ import dogHome from '../../assets/images/dog-home.gif'
 
 export default function About() {
   return (
-    <div className='resources'>
-      <div className='resources__container'>
+    <div className="resources">
+      <div className="resources__container">
         <h1>Bringing home a new pup?</h1>
-        <img src={ dogHome } alt='happy pup on the way home' />
-        <p className='resources__text'>
-          Whether adopting or getting a new puppy, it's a big change for everyone! Here are a few helpful tips:
-          <br/>
-          <br/>
-          <a className='link__text' href="https://thecottagemarket.com/how-to-help-adopted-dogs-adjust-and-relax-in-their-new-home/" target="_blank">Helping Your New Pup Adjust</a>
-         
+        <img src={dogHome} alt="happy pup on the way home" />
+        <p className="resources__text">
+          Whether adopting a rescue or getting a new puppy, it's a big change for
+          everyone! Here are a few helpful tips:
+          <br />
+          <br />
+          <a
+            className="link__text"
+            href="https://thecottagemarket.com/how-to-help-adopted-dogs-adjust-and-relax-in-their-new-home/"
+            target="_blank"
+          >
+            Helping Your New Pup Adjust
+          </a>
+          <br />
+          <a
+            className="link__text"
+            href="https://www.aspcapetinsurance.com/resources/how-much-exercise-does-my-dog-need/"
+            target="_blank"
+          >
+            Good Dogs Get Enough Exercise
+          </a>
+          <br />
+          <a
+            className="link__text"
+            href="https://www.rescuedogs101.com/bringing-new-dog-home-3-3-3-rule/"
+            target="_blank"
+          >
+            The 3-3-3 Rule of Adoption
+          </a>
+          <br />
+          <a
+            className="link__text"
+            href="https://www.akc.org/dog-breeds/"
+            target="_blank"
+          >
+            Full List of AKC Breeds
+          </a>
         </p>
-       
       </div>
     </div>
   )
